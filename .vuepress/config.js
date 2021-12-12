d44dcd16075fe232d692fd9c11fd1c0b4329e337
Yueming Fang <fangyueming@foxmail.com>
2021-12-12 19:41:03 +0800

    feat: update hero

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,7 @@ const { config } = require('vuepress-theme-hope');
 
 module.exports = config({
   title: '方跃明的博客',
-  description: '分享软件应用开发技术',
+  description: '分享WEB应用开发技术',
   dest: './docs',
   head: [
     // ['script', { src: '//cdn.jsdelivr.net/npm/react/umd/react.production.min.js' }],
@@ -16,6 +16,10 @@ module.exports = config({
       lang: "zh-CN",
     },
   },
+  // comment: {
+  //   type: 'waline',
+  //   serverURL: 'https://vuepress-theme-hope-comment.vercel.app',
+  // },
   themeConfig: {
     author: '方跃明',
     hostname: 'https://blog.fangyueming.cn',
@@ -25,21 +29,6 @@ module.exports = config({
       { text: '首页', link: '/', icon: 'home' },
       { text: '码云', link: 'https://gitee.com/axolo', icon: 'gitee' },
     ],
-    // sidebar: {
-    //   '/': [
-    //     '',
-    //     'home',
-    //     'slides',
-    //     'layout',
-    //     {
-    //       title: 'Guide',
-    //       icon: 'creative',
-    //       prefix: 'guide/',
-    //       children: ['', 'page', 'markdown', 'disable', 'encrypt'],
-    //     },
-    //   ],
-    // },
-
     blog: {
       intro: '/intro/',
       sidebarDisplay: 'mobile',
@@ -50,30 +39,17 @@ module.exports = config({
         Weibo: 'https://weibo.com/u/2129355705',
       },
     },
-
     footer: {
       display: true,
-      content: '默认页脚',
+      content: '<a href="http://beian.miit.gov.cn/" target="_blank">浙ICP备20010733号-2</a>',
     },
-
-    comment: {
-      type: 'waline',
-      serverURL: 'https://vuepress-theme-hope-comment.vercel.app',
-    },
-
-    icp: {
-      beian: '浙ICP备20010733号-2',
-      link: 'http://beian.miit.gov.cn/',
-    },
-
     copyright: {
       status: 'global',
     },
-
     git: {
       timezone: 'Asia/Shanghai',
+      contributor: false,
     },
-
     mdEnhance: {
       enableAll: true,
       presentation: {
--- README.md
+++ README.md
@@ -3,9 +3,9 @@ blog: true
 home: true
 icon: home
 title: 博客主页
+bgImage: /hero.svg
 heroImage: /logo.svg
 heroText: 方跃明的博客
-heroFullScreen: true
-tagline: 分享软件应用开发技术
-footer: 分享软件应用开发技术
+heroFullScreen: false
+tagline: 分享WEB应用开发技术
 ---
