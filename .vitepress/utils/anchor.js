9d9823a603affccf8656aefc1d93680d933cc33b
Yueming Fang <fangyueming@foxmail.com>
2023-02-01 16:30:47 +0800

    feat: hash替换.符合vitepress本身规则

--- .vitepress/utils/anchor.js
+++ .vitepress/utils/anchor.js
@@ -1,5 +1,5 @@
 export default id => {
   if ([undefined, null, ''].includes(id)) return id
   const s = isNaN(parseFloat(id)) ? id : ('_' + id)
-  return s.replace(/[\s+|.]/g, '_')
+  return s.replace(/[\s+|.]/g, '-')
 }
