8ae2b927fa654ddd2e5b7445b0f43b5b38c9b74e
Yueming Fang <fangyueming@foxmail.com>
2023-02-06 17:26:26 +0800

    feat: 使用 github actions 发布

--- docs/scripts/baidu-hm.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f8d99518b67e05cc06d5f14a3e5492ce";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
