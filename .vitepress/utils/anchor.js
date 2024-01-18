99e5062f3c0ed99f8117015db2ab344d9934e216
Yueming Fang <fangyueming@foxmail.com>
2024-01-18 11:15:55 +0800

    fix: 修复标签定位失焦问题 close #3

--- .vitepress/utils/anchor.js
+++ .vitepress/utils/anchor.js
@@ -1,5 +1,5 @@
 export default id => {
   if ([undefined, null, ''].includes(id)) return id
   const s = isNaN(parseFloat(id)) ? id : ('_' + id)
-  return s.replace(/[\s+|.]/g, '-')
+  return s.replace(/[\s+|.]/g, '-').toLowerCase()
 }
